310548f47747c6d8d24beeabc0a7825173e91d90
Jong Shin <jshin.m@gmail.com>
2023-01-17 23:34:55 -0500

    Fix docstring for `node_value`

--- sktree/tree/_unsup_criterion.pyx
+++ sktree/tree/_unsup_criterion.pyx
@@ -42,59 +42,27 @@ cdef class UnsupervisedCriterion(BaseCriterion):
 
     cdef int init(
         self,
-        const DOUBLE_t[:,:] X,
+        const DTYPE_t[:,:] X,
         const DOUBLE_t[:] sample_weight,
         double weighted_n_samples,
         const SIZE_t[:] sample_indices
     ) nogil except -1:
-        """Initialize the criterion.
-
-        This initializes the criterion at node samples[start:end] and children
-        samples[start:start] and samples[start:end].
+        """Placehodler for a method which will initialize the unsuperivsed criterion
 
         Returns -1 in case of failure to allocate memory (and raise MemoryError)
         or 0 otherwise.
 
         Parameters
         ----------
-        X : array-like, dtype=DOUBLE_t
+        X : array-like, dtype=DTYPE_t
             The data-feature matrix stored as a buffer for memory efficiency. Note that
             this is not used, but simply passed as a convenience function.
         sample_weight : array-like, dtype=DOUBLE_t
             The weight of each sample (i.e. row of X).
         weighted_n_samples : double
             The total weight of all samples.
-        samples : array-like, dtype=SIZE_t
-            A mask on the samples, showing which ones we want to use
         """
-        self.sample_weight = sample_weight
-        self.samples = samples
-        self.start = start
-        self.end = end
-        self.n_node_samples = end - start
-        self.weighted_n_samples = weighted_n_samples
-        self.weighted_n_node_samples = 0.0
-
-        cdef SIZE_t i
-        cdef SIZE_t p
-        cdef DOUBLE_t w = 1.0
-        self.sum_total = 0.0
-
-        for p in range(start, end):
-            i = samples[p]
-
-            # w is originally set to be 1.0, meaning that if no sample weights
-            # are given, the default weight of each sample is 1.0
-            if sample_weight != NULL:
-                w = sample_weight[i]
-            
-            # ith sample; overall compute the weighted average
-            self.sum_total += w * X[i]
-            self.weighted_n_node_samples += w
-
-        # Reset to pos=start
-        self.reset()
-        return 0
+        pass
 
 
 cdef class TwoMeans(UnsupervisedCriterion):
@@ -132,7 +100,6 @@ cdef class TwoMeans(UnsupervisedCriterion):
     pair minimizes the splitting criteria described in the following 
     section
     """
-
     cdef int init(
         self,
         const DTYPE_t[:,:] X,
@@ -367,10 +334,7 @@ cdef class TwoMeans(UnsupervisedCriterion):
         pass
 
     cdef void node_value(self, double* dest) nogil:
-        r"""Compute the node value of samples[start:end] and save it into dest.
-
-        TODO: No, it does not handle the computation. 
-        See scikit-learn splitter and tree for how node_value() is used.
+        r"""Set the node value with sum_total and save it into dest.
 
         Parameters
         ----------
