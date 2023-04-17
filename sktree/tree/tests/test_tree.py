82c7f5b65d8f881698c963fc1dad8a8336e8f2d1
SUKI <Ituki82@gmail.com>
2023-04-17 19:43:46 +0900

    WIP: add tests, docstrings

--- sktree/tree/tests/test_tree.py
+++ sktree/tree/tests/test_tree.py
@@ -196,6 +196,27 @@ def assert_tree_equal(d, s, message):
         d.value[external], s.value[external], err_msg=message + ": inequal value"
     )
 
+@pytest.mark.parametrize("Tree", REG_TREES.values())
+@pytest.mark.parametrize("criterion", REG_CRITERIONS)
+def test_regression_toy(Tree, criterion):
+    # Check regression on a toy dataset.
+    if criterion == "poisson":
+        # make target positive while not touching the original y and
+        # true_result
+        a = np.abs(np.min(y)) + 1
+        y_train = np.array(y) + a
+        y_test = np.array(true_result) + a
+    else:
+        y_train = y
+        y_test = true_result
+
+    reg = Tree(criterion=criterion, random_state=1)
+    reg.fit(X, y_train)
+    assert_allclose(reg.predict(T), y_test)
+
+    clf = Tree(criterion=criterion, max_features=1, random_state=1)
+    clf.fit(X, y_train)
+    assert_allclose(reg.predict(T), y_test)
 
 @pytest.mark.parametrize("Tree", REG_TREES.values())
 @pytest.mark.parametrize("criterion", REG_CRITERIONS)
@@ -547,7 +568,6 @@ def test_diabetes_overfit(name, Tree, criterion):
     assert score == pytest.approx(
         0
     ), f"Failed with {name}, criterion = {criterion} and score = {score}"
-
 
 @skip_if_32bit
 @pytest.mark.parametrize("name, Tree", REG_TREES.items())
