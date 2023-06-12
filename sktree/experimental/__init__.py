3e63af5c1f8bcaff4b0a9e235aaf65de07b00e51
Adam Li <adam2392@gmail.com>
2023-06-12 13:12:42 -0400

    Add experiemntal subdir
    Signed-off-by: Adam Li <adam2392@gmail.com>

---
+++ sktree/experimental/__init__.py
@@ -0,0 +1,10 @@
+from .mutual_info import (
+    entropy_gaussian,
+    entropy_weibull,
+    mi_gaussian,
+    mi_gamma,
+    cmi_gaussian,
+    mutual_info_ksg,
+    mi_from_entropy,
+    cmi_from_entropy,
+)
