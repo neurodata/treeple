241786249bbc6a784dbacebcb8033faf1d793fba
Adam Li <adam2392@gmail.com>
2023-08-25 13:47:59 -0400

    Add fix to oblique forest and patch oblique forest classifier
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- sktree/tests/test_supervised_forest.py
+++ sktree/tests/test_supervised_forest.py
@@ -187,10 +187,10 @@ def _trunk(n, p=10, random_state=None):
 )
 def test_sklearn_compatible_estimator(estimator, check):
     # TODO: remove when we can replicate the CI error...
-    if isinstance(
-        estimator, (ObliqueRandomForestClassifier, PatchObliqueRandomForestClassifier)
-    ) and check.func.__name__ in ["check_fit_score_takes_y"]:
-        pytest.skip()
+    # if isinstance(
+    #     estimator, (ObliqueRandomForestClassifier, PatchObliqueRandomForestClassifier)
+    # ) and check.func.__name__ in ["check_fit_score_takes_y"]:
+    #     pytest.skip()
     check(estimator)
 
 
