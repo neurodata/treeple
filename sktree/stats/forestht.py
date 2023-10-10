ddaeb5f2826d00b3ec69ecdab3e1bfeb43d0c8ab
Sambit Panda <sampanda501@gmail.com>
2023-10-10 14:37:12 -0400

    fix docstring

--- sktree/stats/forestht.py
+++ sktree/stats/forestht.py
@@ -429,7 +429,7 @@ class BaseForestHT(MetaEstimatorMixin):
         y : ArrayLike of shape (n_samples, n_outputs)
             The target matrix.
         covariate_index : ArrayLike, optional of shape (n_covariates,)
-            The index array of covariates to shuffle, by default None.
+            The index array of covariates to shuffle, will shuffle all columns by default.
         metric : str, optional
             The metric to compute, by default "mse".
         n_repeats : int, optional
