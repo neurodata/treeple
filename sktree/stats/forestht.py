ad49c88d79e8edcdd5c8edb6f2669f063abc1018
Adam Li <adam2392@gmail.com>
2023-11-14 13:03:39 -0500

    Fix unit-tests
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- sktree/stats/forestht.py
+++ sktree/stats/forestht.py
@@ -382,50 +382,28 @@ class BaseForestHT(MetaEstimatorMixin):
             self.permuted_estimator_ = self._get_estimator()
             estimator = self.permuted_estimator_
 
-            if not hasattr(self, "estimator_") or self.estimator_ is None:
-                self.estimator_ = self._get_estimator()
-
-                # Ensure that the estimator_ is fitted at least
-                if not _is_fitted(self.estimator_) and is_classifier(self.estimator_):
-                    _unique_y = []
-                    for axis in range(y.shape[1]):
-                        _unique_y.append(np.unique(y[:, axis]))
-                    unique_y = np.hstack(_unique_y)
-                    if unique_y.ndim > 1 and unique_y.shape[1] == 1:
-                        unique_y = unique_y.ravel()
-                    X_dummy = np.zeros((unique_y.shape[0], X.shape[1]))
-                    self.estimator_.fit(X_dummy, unique_y)
-                elif not _is_fitted(estimator):
-                    if y.ndim > 1 and y.shape[1] == 1:
-                        self.estimator_.fit(X[:2], y[:2].ravel())
-                    else:
-                        self.estimator_.fit(X[:2], y[:2])
-
         # Store a cache of the y variable
-        if is_classifier(self._get_estimator()):
+        if is_classifier(estimator):
             self._y = y.copy()
 
-        # XXX: this can be improved as an extra fit can be avoided, by just doing error-checking
-        # and then setting the internal meta data structures
-        # first run a dummy fit on the samples to initialize the
-        # internal data structure of the forest
-        if not hasattr(self, "estimator_") or self.estimator_ is None:
-            self.estimator_ = self._get_estimator()
-
-            if not _is_fitted(estimator) and is_classifier(estimator):
-                _unique_y = []
-                for axis in range(y.shape[1]):
-                    _unique_y.append(np.unique(y[:, axis]))
-                unique_y = np.hstack(_unique_y)
-                if unique_y.ndim > 1 and unique_y.shape[1] == 1:
-                    unique_y = unique_y.ravel()
-                X_dummy = np.zeros((unique_y.shape[0], X.shape[1]))
-                estimator.fit(X_dummy, unique_y)
-            elif not _is_fitted(estimator):
-                if y.ndim > 1 and y.shape[1] == 1:
-                    estimator.fit(X[:2], y[:2].ravel())
-                else:
-                    estimator.fit(X[:2], y[:2])
+        # # XXX: this can be improved as an extra fit can be avoided, by just doing error-checking
+        # # and then setting the internal meta data structures
+        # # first run a dummy fit on the samples to initialize the
+        # # internal data structure of the forest
+        if not _is_fitted(estimator) and is_classifier(estimator):
+            _unique_y = []
+            for axis in range(y.shape[1]):
+                _unique_y.append(np.unique(y[:, axis]))
+            unique_y = np.hstack(_unique_y)
+            if unique_y.ndim > 1 and unique_y.shape[1] == 1:
+                unique_y = unique_y.ravel()
+            X_dummy = np.zeros((unique_y.shape[0], X.shape[1]))
+            estimator.fit(X_dummy, unique_y)
+        elif not _is_fitted(estimator):
+            if y.ndim > 1 and y.shape[1] == 1:
+                estimator.fit(X[:2], y[:2].ravel())
+            else:
+                estimator.fit(X[:2], y[:2])
 
         # sampling a separate train/test per tree
         if self.sample_dataset_per_tree:
