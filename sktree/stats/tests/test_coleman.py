b2b64d339915c3aaf2f66e5efa99976e8c2ccb4a
Adam Li <adam2392@gmail.com>
2023-11-14 20:25:12 -0500

    Fix unit test
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- sktree/stats/tests/test_coleman.py
+++ sktree/stats/tests/test_coleman.py
@@ -79,8 +79,9 @@ rng = np.random.default_rng(seed)
             {
                 "estimator": RandomForestRegressor(
                     max_features="sqrt",
-                    n_estimators=200,
+                    n_estimators=250,
                     n_jobs=-1,
+                    random_state=rng.integers(0, 1000),
                 ),
                 "random_state": seed,
                 "permute_forest_fraction": 0.5,
@@ -198,10 +199,11 @@ def test_linear_model(hypotester, model_kwargs, n_samples, n_repeats, test_size)
                     max_features="sqrt",
                     n_estimators=200,
                     n_jobs=-1,
+                    random_state=rng.integers(0, 1000),
                 ),
                 "permute_forest_fraction": 0.5,
                 "sample_dataset_per_tree": False,
-                "random_state": seed,
+                "random_state": rng.integers(0, 1000),
             },
             600,  # n_samples
             1000,  # n_repeats
