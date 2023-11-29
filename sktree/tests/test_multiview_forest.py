2a52c40401967c3b4c19acaf94763e09a52ab9b0
Adam Li <adam2392@gmail.com>
2023-11-29 18:41:45 -0500

    Fix bug in max features sampling
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- sktree/tests/test_multiview_forest.py
+++ sktree/tests/test_multiview_forest.py
@@ -1,11 +1,13 @@
 import numpy as np
 import pytest
+from numpy.testing import assert_array_equal
 from sklearn.datasets import make_blobs
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.utils.estimator_checks import parametrize_with_checks
 
 from sktree import MultiViewRandomForestClassifier, RandomForestClassifier
+from sktree.datasets import make_joint_factor_model
 
 seed = 12345
 
@@ -87,3 +89,83 @@ def test_multiview_classification(baseline_est):
     assert (
         cross_val_score(clf, X, y, cv=5).mean() <= 0.95
     ), f"CV score: {cross_val_score(clf, X, y, cv=5).mean()}"
+
+
+@pytest.mark.parametrize(
+    "n_views, max_features",
+    [
+        (2, 0.3),
+        (3, 0.3),
+        (4, 0.3),
+        (5, 0.3),
+    ],
+)
+def test_three_view_dataset(n_views, max_features):
+    rng = np.random.default_rng(seed=seed)
+
+    n_estimators = 50
+
+    n_samples = 100
+    n_views = 5
+    n_features = 200
+
+    # number of dimensions that are informative
+    n_signals = 5
+
+    # Create a high-dimensional multiview dataset
+    Xs = make_joint_factor_model(
+        n_features=n_signals,
+        m=2.0,
+        n_views=n_views,
+        n_samples=n_samples,
+        joint_rank=5,
+        random_state=seed + 1,
+    )
+    X = np.empty((n_samples, 0))
+    feature_set_ends = []
+    for _X in Xs:
+        X = np.hstack((X, _X, rng.standard_normal(size=(n_samples, n_features - n_signals))))
+        feature_set_ends.append(X.shape[1])
+
+    Xs = make_joint_factor_model(
+        n_features=n_signals,
+        m=2.0,
+        n_views=n_views,
+        n_samples=n_samples,
+        joint_rank=5,
+        random_state=seed + 2,
+    )
+    second_X = np.empty((n_samples, 0))
+    for _X in Xs:
+        second_X = np.hstack(
+            (second_X, _X, rng.standard_normal(size=(n_samples, n_features - n_signals)))
+        )
+    X = np.vstack((X, second_X))
+    y = np.concatenate([np.zeros(n_samples), np.ones(n_samples)]).T
+
+    # perform train/test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=seed)
+
+    # Compare multiview decision tree vs single-view decision tree
+    clf = MultiViewRandomForestClassifier(
+        random_state=seed,
+        feature_set_ends=feature_set_ends,
+        apply_max_features_per_feature_set=True,
+        max_features=max_features,
+        n_estimators=n_estimators,
+    )
+    clf.fit(X_train, y_train)
+
+    rf_clf = RandomForestClassifier(
+        random_state=seed,
+        max_features=max_features,
+        n_estimators=n_estimators,
+    )
+    rf_clf.fit(X_train, y_train)
+    assert_array_equal(
+        clf.estimators_[0].max_features_per_set_, [n_features * max_features] * n_views
+    )
+
+    rf_acc = accuracy_score(y_test, rf_clf.predict(X_test))
+    acc = accuracy_score(y_test, clf.predict(X_test))
+    assert acc > rf_acc + 0.01, f"Accuracy score: {acc} < {rf_acc}"
