a680d68d3c0796293e9f5b02b7a9eacadd05cbc8
Sambit Panda <sampanda501@gmail.com>
2024-02-19 23:45:34 -0500

    fix parameter names

--- sktree/datasets/hyppo.py
+++ sktree/datasets/hyppo.py
@@ -261,7 +261,7 @@ def make_trunk_classification(
         )
 
     if n_dim > n_informative:
-        X = np.hstack((X, rng.normal(low=0, high=1, size=(X.shape[0], n_dim - n_informative))))
+        X = np.hstack((X, rng.normal(loc=0, scale=1, size=(X.shape[0], n_dim - n_informative))))
 
     y = np.concatenate((np.zeros(n_samples // 2), np.ones(n_samples // 2)))
 
