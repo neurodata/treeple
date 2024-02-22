7b9edfedfac4249b9acb84c22bfaf800f1f50892
Adam Li <adam2392@gmail.com>
2024-02-22 14:26:54 -0500

    Fix
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- sktree/ensemble/_honest_forest.py
+++ sktree/ensemble/_honest_forest.py
@@ -3,25 +3,19 @@
 
 import threading
 from numbers import Integral
-from warnings import catch_warnings, simplefilter, warn
+from warnings import catch_warnings, simplefilter
 
 import numpy as np
 from joblib import Parallel, delayed
-from scipy.sparse import issparse
-from sklearn.base import _fit_context
-from sklearn.ensemble._base import _partition_estimators
-from sklearn.ensemble._hist_gradient_boosting.binning import _BinMapper
-from sklearn.exceptions import DataConversionWarning
-from sklearn.utils import check_random_state, compute_sample_weight
-from sklearn.utils._openmp_helpers import _openmp_effective_n_threads
+from sklearn.base import _fit_context, clone
+from sklearn.ensemble._base import _partition_estimators, _set_random_states
+from sklearn.utils import compute_sample_weight
 from sklearn.utils._param_validation import Interval, RealNotInt
-from sklearn.utils.multiclass import type_of_target
-from sklearn.utils.validation import _check_sample_weight, check_is_fitted
+from sklearn.utils.validation import check_is_fitted
 
 from .._lib.sklearn.ensemble._forest import ForestClassifier
-from .._lib.sklearn.tree._tree import DOUBLE, DTYPE
 from ..tree import HonestTreeClassifier
-from ._extensions import ForestClassifierMixin, _generate_sample_indices, _get_n_samples_bootstrap
+from ._extensions import ForestClassifierMixin, _generate_sample_indices
 
 
 def _parallel_build_trees(
@@ -419,27 +413,30 @@ class HonestForestClassifier(ForestClassifier, ForestClassifierMixin):
         honest_fraction=0.5,
         tree_estimator=None,
         stratify=False,
+        **tree_estimator_params,
     ):
+        estimator_params = (
+            "criterion",
+            "splitter",
+            "max_depth",
+            "min_samples_split",
+            "min_samples_leaf",
+            "min_weight_fraction_leaf",
+            "max_features",
+            "max_leaf_nodes",
+            "min_impurity_decrease",
+            "random_state",
+            "ccp_alpha",
+            "tree_estimator",
+            "honest_fraction",
+            "honest_prior",
+            "stratify",
+        )
+
         super().__init__(
             estimator=HonestTreeClassifier(),
             n_estimators=n_estimators,
-            estimator_params=(
-                "criterion",
-                "splitter",
-                "max_depth",
-                "min_samples_split",
-                "min_samples_leaf",
-                "min_weight_fraction_leaf",
-                "max_features",
-                "max_leaf_nodes",
-                "min_impurity_decrease",
-                "random_state",
-                "ccp_alpha",
-                "tree_estimator",
-                "honest_fraction",
-                "honest_prior",
-                "stratify",
-            ),
+            estimator_params=estimator_params,
             bootstrap=bootstrap,
             oob_score=oob_score,
             n_jobs=n_jobs,
@@ -463,9 +460,10 @@ class HonestForestClassifier(ForestClassifier, ForestClassifierMixin):
         self.honest_prior = honest_prior
         self.tree_estimator = tree_estimator
         self.stratify = stratify
+        self._tree_estimator_params = tree_estimator_params
 
     @_fit_context(prefer_skip_nested_validation=True)
-    def fit(self, X, y, sample_weight=None, classes=None):
+    def fit(self, X, y, sample_weight=None, classes=None, **fit_params):
         """
         Build a forest of trees from the training set (X, y).
 
@@ -490,195 +488,22 @@ class HonestForestClassifier(ForestClassifier, ForestClassifierMixin):
         classes : array-like of shape (n_classes,), default=None
             List of all the classes that can possibly appear in the y vector.
 
+        **fit_params : dict
+            Parameters to pass to the underlying base tree estimators.
+
+                Only available if `enable_metadata_routing=True`,
+                which can be set by using
+                ``sklearn.set_config(enable_metadata_routing=True)``.
+                See :ref:`Metadata Routing User Guide <metadata_routing>` for
+                more details.
+
         Returns
         -------
         self : object
             Fitted estimator.
         """
-        # XXX: This entire function is a copy of what is in scikit-learn
-        # with the exception of:
-        # - _get_n_samples_bootstrap is a re-defined function to allow higher
-        #   max_samples
 
-        MAX_INT = np.iinfo(np.int32).max
-        # Validate or convert input data
-        if issparse(y):
-            raise ValueError("sparse multilabel-indicator for y is not supported.")
-
-        X, y = self._validate_data(
-            X,
-            y,
-            multi_output=True,
-            accept_sparse="csc",
-            dtype=DTYPE,
-            force_all_finite=False,
-        )
-        # _compute_missing_values_in_feature_mask checks if X has missing values and
-        # will raise an error if the underlying tree base estimator can't handle missing
-        # values. Only the criterion is required to determine if the tree supports
-        # missing values.
-        estimator = type(self.estimator)(criterion=self.criterion)
-        missing_values_in_feature_mask = estimator._compute_missing_values_in_feature_mask(
-            X, estimator_name=self.__class__.__name__
-        )
-
-        if sample_weight is not None:
-            sample_weight = _check_sample_weight(sample_weight, X)
-
-        if issparse(X):
-            # Pre-sort indices to avoid that each individual tree of the
-            # ensemble sorts the indices.
-            X.sort_indices()
-
-        y = np.atleast_1d(y)
-        if y.ndim == 2 and y.shape[1] == 1:
-            warn(
-                (
-                    "A column-vector y was passed when a 1d array was"
-                    " expected. Please change the shape of y to "
-                    "(n_samples,), for example using ravel()."
-                ),
-                DataConversionWarning,
-                stacklevel=2,
-            )
-
-        if y.ndim == 1:
-            # reshape is necessary to preserve the data contiguity against vs
-            # [:, np.newaxis] that does not.
-            y = np.reshape(y, (-1, 1))
-
-        if self.criterion == "poisson":
-            if np.any(y < 0):
-                raise ValueError(
-                    "Some value(s) of y are negative which is "
-                    "not allowed for Poisson regression."
-                )
-            if np.sum(y) <= 0:
-                raise ValueError(
-                    "Sum of y is not strictly positive which "
-                    "is necessary for Poisson regression."
-                )
-
-        self._n_samples, self.n_outputs_ = y.shape
-
-        y, expanded_class_weight = self._validate_y_class_weight(y, classes=classes)
-
-        if getattr(y, "dtype", None) != DOUBLE or not y.flags.contiguous:
-            y = np.ascontiguousarray(y, dtype=DOUBLE)
-
-        if expanded_class_weight is not None:
-            if sample_weight is not None:
-                sample_weight = sample_weight * expanded_class_weight
-            else:
-                sample_weight = expanded_class_weight
-
-        # compute the number of samples we want to sample for each tree
-        # possibly without replacement
-        n_samples_bootstrap = _get_n_samples_bootstrap(
-            n_samples=X.shape[0], max_samples=self.max_samples
-        )
-
-        self._n_samples_bootstrap = n_samples_bootstrap
-
-        self._validate_estimator()
-
-        if not self.bootstrap and self.oob_score:
-            raise ValueError("Out of bag estimation only available if bootstrap=True")
-
-        random_state = check_random_state(self.random_state)
-
-        if not self.warm_start or not hasattr(self, "estimators_"):
-            # Free allocated memory, if any
-            self.estimators_ = []
-
-        n_more_estimators = self.n_estimators - len(self.estimators_)
-
-        if self.max_bins is not None:
-            # `_openmp_effective_n_threads` is used to take cgroups CPU quotes
-            # into account when determine the maximum number of threads to use.
-            n_threads = _openmp_effective_n_threads()
-
-            # Bin the data
-            # For ease of use of the API, the user-facing GBDT classes accept the
-            # parameter max_bins, which doesn't take into account the bin for
-            # missing values (which is always allocated). However, since max_bins
-            # isn't the true maximal number of bins, all other private classes
-            # (binmapper, histbuilder...) accept n_bins instead, which is the
-            # actual total number of bins. Everywhere in the code, the
-            # convention is that n_bins == max_bins + 1
-            n_bins = self.max_bins + 1  # + 1 for missing values
-            self._bin_mapper = _BinMapper(
-                n_bins=n_bins,
-                # is_categorical=self.is_categorical_,
-                known_categories=None,
-                random_state=random_state,
-                n_threads=n_threads,
-            )
-
-            # XXX: in order for this to work with the underlying tree submodule's Cython
-            # code, we need to convert this into the original data's DTYPE because
-            # the Cython code assumes that `DTYPE` is used.
-            # The proper implementation will be a lot more complicated and should be
-            # tackled once scikit-learn has finalized their inclusion of missing data
-            # and categorical support for decision trees
-            X = self._bin_data(X, is_training_data=True)  # .astype(DTYPE)
-        else:
-            self._bin_mapper = None
-
-        if n_more_estimators < 0:
-            raise ValueError(
-                "n_estimators=%d must be larger or equal to "
-                "len(estimators_)=%d when warm_start==True"
-                % (self.n_estimators, len(self.estimators_))
-            )
-
-        elif n_more_estimators == 0:
-            warn("Warm-start fitting without increasing n_estimators does not " "fit new trees.")
-        else:
-            if self.warm_start and len(self.estimators_) > 0:
-                # We draw from the random state to get the random state we
-                # would have got if we hadn't used a warm_start.
-                random_state.randint(MAX_INT, size=len(self.estimators_))
-
-            trees = self._construct_trees(
-                X,
-                y,
-                sample_weight,
-                random_state,
-                n_samples_bootstrap,
-                missing_values_in_feature_mask,
-                classes,
-                n_more_estimators,
-            )
-
-            # Collect newly grown trees
-            self.estimators_.extend(trees)
-
-        if self.oob_score and (n_more_estimators > 0 or not hasattr(self, "oob_score_")):
-            y_type = type_of_target(y)
-            if y_type == "unknown" or (
-                self._estimator_type == "classifier" and y_type == "multiclass-multioutput"
-            ):
-                # FIXME: we could consider to support multiclass-multioutput if
-                # we introduce or reuse a constructor parameter (e.g.
-                # oob_score) allowing our user to pass a callable defining the
-                # scoring strategy on OOB sample.
-                raise ValueError(
-                    "The type of target cannot be used to compute OOB "
-                    f"estimates. Got {y_type} while only the following are "
-                    "supported: continuous, continuous-multioutput, binary, "
-                    "multiclass, multilabel-indicator."
-                )
-
-            if callable(self.oob_score):
-                self._set_oob_score_and_attributes(X, y, scoring_function=self.oob_score)
-            else:
-                self._set_oob_score_and_attributes(X, y)
-
-        # Decapsulate classes_ attributes
-        if hasattr(self, "classes_") and self.n_outputs_ == 1:
-            self.n_classes_ = self.n_classes_[0]
-            self.classes_ = self.classes_[0]
+        super().fit(X, y, sample_weight=sample_weight, classes=classes, **fit_params)
 
         # Compute honest decision function
         self.honest_decision_function_ = self._predict_proba(
@@ -686,6 +511,27 @@ class HonestForestClassifier(ForestClassifier, ForestClassifierMixin):
         )
 
         return self
+
+    def _make_estimator(self, append=True, random_state=None):
+        """Make and configure a copy of the `estimator_` attribute.
+
+        Warning: This method should be used to properly instantiate new
+        sub-estimators.
+        """
+        estimator = clone(self.estimator_)
+        estimator.set_params(**{p: getattr(self, p) for p in self.estimator_params})
+
+        # XXX: This is the only change compared to scikit-learn's make_estimator
+        # additionally set the tree_estimator_params
+        estimator._tree_estimator_params = self._tree_estimator_params
+
+        if random_state is not None:
+            _set_random_states(estimator, random_state)
+
+        if append:
+            self.estimators_.append(estimator)
+
+        return estimator
 
     def _construct_trees(
         self,
