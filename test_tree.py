ed1b3e2b309ad00231e233e68e2b0e3afa1549ce
Adam Li <adam2392@gmail.com>
2024-04-02 19:12:54 -0400

    REmove
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- test_tree.py
+++
@@ -1,7 +0,0 @@
-from sklearn.utils.estimator_checks import check_fit_score_takes_y
-
-from sktree.tree import MultiViewDecisionTreeClassifier
-
-est = MultiViewDecisionTreeClassifier()
-est.partial_fit
-check_fit_score_takes_y("mvtree", MultiViewDecisionTreeClassifier())
