964dd255fb89aa0c7a84d7b876bd17f4cad1baae
Haoyin Xu <haoyinxu@gmail.com>
2024-04-17 12:39:30 -0400

    ENH add treeple tutorials to website (#256)
    * ENH add treeple tutorials
    
    * ENH update the notebook cells
    
    * FIX modify import order
    
    * DOC optimize the text for tree permutations
    
    * DOC add docstrings to local functions
    
    * FIX correct spelling
    
    * ENH attempt to add notebook support
    
    * TST try p-value example
    
    * STY fix style
    
    * FIX remove unnecessary file
    
    * FIX update script structure
    
    * DOC upload other converted tutorials
    
    * DOC correct spelling
    
    * FIX correct import & spelling
    
    * FIX add local function
    
    * STY update black formatting
    
    * DOC move to original folder & address comments
    
    * Update examples/treeple/treeple_tutorial_pvalue_multiview.py
    
    Co-authored-by: Adam Li <adam2392@gmail.com>
    
    ---------
    
    Co-authored-by: Adam Li <adam2392@gmail.com>

---
+++ examples/treeple/treeple_tutorial_CMI.py
@@ -0,0 +1,149 @@
+"""
+====================================
+Treeple tutorial for calculating CMI
+====================================
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import entropy
+
+from sktree.datasets import make_trunk_classification
+from sktree.ensemble import HonestForestClassifier
+from sktree.stats import build_hyppo_oob_forest
+from sktree.tree import MultiViewDecisionTreeClassifier
+
+# %%
+# CMI
+# ---
+#
+# Conditional mutual information (*CMI*) measures the dependence of *Y* on
+# *X* conditioned on *Z*. It can be calculated by the difference between
+# the joint MI (``I([X, Z]; Y)``) and the MI on Z (``I(Y; Z)``):
+#
+# .. math:: I(X; Y | Z) = I([X, Z]; Y) - I(Y; Z)
+#
+# With a multiview binary class simulation as an example, this tutorial
+# will show how to use ``treeple`` to calculate the statistic with
+# multiview data.
+
+# %%
+# Create a simulation with two gaussians
+# --------------------------------------
+
+
+# create a binary class simulation with two gaussians
+# 500 samples for each class, class zero is standard
+# gaussian, and class one has a mean at one for Z
+Z, y = make_trunk_classification(
+    n_samples=1000,
+    n_dim=1,
+    mu_0=0,
+    mu_1=1,
+    n_informative=1,
+    seed=1,
+)
+
+
+X, y = make_trunk_classification(
+    n_samples=1000,
+    n_dim=1,
+    mu_0=0,
+    mu_1=2,
+    n_informative=1,
+    seed=1,
+)
+
+
+# scatter plot the samples for Z
+plt.hist(Z[500:], bins=15, alpha=0.6, color="blue", label="negative")
+plt.hist(Z[:500], bins=15, alpha=0.6, color="red", label="positive")
+plt.legend()
+plt.show()
+
+
+# scatter plot the samples for X
+plt.hist(X[500:], bins=15, alpha=0.6, color="blue", label="negative")
+plt.hist(X[:500], bins=15, alpha=0.6, color="red", label="positive")
+plt.legend()
+plt.show()
+
+
+# %%
+# Fit the model with X and Z
+# --------------------------
+
+
+# initialize the forest with 100 trees
+est = HonestForestClassifier(
+    n_estimators=100,
+    max_samples=1.6,
+    max_features=0.3,
+    bootstrap=True,
+    stratify=True,
+    tree_estimator=MultiViewDecisionTreeClassifier(),
+    random_state=1,
+)
+
+# fit the model and obtain the tree posteriors
+_, observe_proba = build_hyppo_oob_forest(est, np.hstack((X, Z)), y)
+
+# generate forest posteriors for the two classes
+observe_proba = np.nanmean(observe_proba, axis=0)
+
+
+# scatter plot the posterior probabilities for class one
+plt.hist(observe_proba[:500][:, 1], bins=30, alpha=0.6, color="blue", label="negative")
+plt.hist(observe_proba[500:][:, 1], bins=30, alpha=0.6, color="red", label="positive")
+plt.legend()
+plt.show()
+
+# %%
+# Fit the model with Z only
+# -------------------------
+
+
+# initialize the forest with 100 trees
+est = HonestForestClassifier(
+    n_estimators=100,
+    max_samples=1.6,
+    max_features=0.3,
+    bootstrap=True,
+    stratify=True,
+    random_state=1,
+)
+
+# fit the model and obtain the tree posteriors
+_, single_proba = build_hyppo_oob_forest(est, Z, y)
+
+# generate forest posteriors for the two classes
+single_proba = np.nanmean(single_proba, axis=0)
+
+
+# scatter plot the posterior probabilities for class one
+plt.hist(single_proba[:500][:, 1], bins=30, alpha=0.6, color="blue", label="negative")
+plt.hist(single_proba[500:][:, 1], bins=30, alpha=0.6, color="red", label="positive")
+plt.legend()
+plt.show()
+
+
+# %%
+# Calculate the statistic
+# -----------------------
+
+
+def Calculate_MI(y_true, y_pred_proba):
+    # calculate the conditional entropy
+    H_YX = np.mean(entropy(y_pred_proba, base=np.exp(1), axis=1))
+
+    # empirical count of each class (n_classes)
+    _, counts = np.unique(y_true, return_counts=True)
+    # calculate the entropy of labels
+    H_Y = entropy(counts, base=np.exp(1))
+    return H_Y - H_YX
+
+
+joint_mi = Calculate_MI(y, observe_proba)
+mi = Calculate_MI(y, single_proba)
+
+print("CMI =", round(joint_mi - mi, 2))
---
+++ examples/treeple/treeple_tutorial_pAUC.py
@@ -0,0 +1,119 @@
+"""
+=====================================
+Treeple tutorial for calculating pAUC
+=====================================
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import RocCurveDisplay, roc_auc_score, roc_curve
+
+from sktree.datasets import make_trunk_classification
+from sktree.ensemble import HonestForestClassifier
+from sktree.stats import build_hyppo_oob_forest
+
+# %%
+# pAUC@r
+# ------
+#
+# Partial area under the ROC curve (*pAUC*) integrates the true positive
+# rates (*TPR*) when the false positive rates (*FPR*) are below a specific
+# percentage threshold. Then the value is normalized by that percentage.
+#
+# .. math:: pAUC@r = \frac{100}{100 - r} \int_{T_r}^\infty \int_{\mathcal{X}} \mathbb{I}\{\eta(X_1) > \eta(X_0) \} dF_1 dF_0
+#
+# With a binary class simulation as an example, this tutorial will show
+# how to use ``treeple`` to calculate the statistic with 90% specificity
+# threshold.
+
+# %%
+# Create a simulation with two gaussians
+# --------------------------------------
+
+
+# create a binary class simulation with two gaussians
+# 500 samples for each class, class zero is standard
+# gaussian, and class one has a mean at one
+X, y = make_trunk_classification(
+    n_samples=1000,
+    n_dim=1,
+    mu_0=0,
+    mu_1=1,
+    n_informative=1,
+    seed=1,
+)
+
+
+# scatter plot the samples
+plt.hist(X[500:], bins=15, alpha=0.6, color="blue", label="negative")
+plt.hist(X[:500], bins=15, alpha=0.6, color="red", label="positive")
+plt.legend()
+plt.show()
+
+
+# %%
+# Fit the model
+# -------------
+
+
+# initialize the forest with 100 trees
+est = HonestForestClassifier(
+    n_estimators=100,
+    max_samples=1.6,
+    max_features=0.3,
+    bootstrap=True,
+    stratify=True,
+    random_state=1,
+)
+
+# fit the model and obtain the tree posteriors
+_, observe_proba = build_hyppo_oob_forest(est, X, y)
+
+# generate forest posteriors for the two classes
+observe_proba = np.nanmean(observe_proba, axis=0)
+
+
+# scatter plot the posterior probabilities for class one
+plt.hist(observe_proba[:500][:, 1], bins=30, alpha=0.6, color="blue", label="negative")
+plt.hist(observe_proba[500:][:, 1], bins=30, alpha=0.6, color="red", label="positive")
+plt.legend()
+plt.show()
+
+
+# %%
+# Calculate the statistic
+# -----------------------
+
+
+def Calculate_pAUC(y_true, y_pred_proba, max_fpr=0.1) -> float:
+    # check the shape of true labels
+    if y_true.squeeze().ndim != 1:
+        raise ValueError(f"y_true must be 1d, not {y_true.shape}")
+
+    # find the positive class and calculate fpr and tpr
+    if 0 in y_true or -1 in y_true:
+        fpr, tpr, thresholds = roc_curve(
+            y_true, y_pred_proba[:, 1], pos_label=1, drop_intermediate=False
+        )
+    else:
+        fpr, tpr, thresholds = roc_curve(
+            y_true, y_pred_proba[:, 1], pos_label=2, drop_intermediate=False
+        )
+    RocCurveDisplay(fpr=fpr, tpr=tpr).plot(label="ROC Curve")
+    # Calculate pAUC at the specific threshold
+    pAUC = roc_auc_score(y_true, y_pred_proba[:, 1], max_fpr=max_fpr)
+
+    pos = np.where(fpr == max_fpr)[0][-1]
+    plt.fill_between(
+        fpr[:pos],
+        tpr[:pos],
+        color="r",
+        alpha=0.6,
+        label="pAUC@90 = " + str(round(pAUC, 2)),
+        linestyle="--",
+    )
+    plt.legend()
+    return pAUC
+
+
+pAUC = Calculate_pAUC(y, observe_proba)
