cc9685e6a5b1f533871b5c770f62649ca6f5558c
Ryan Hausen <ryan.hausen@gmail.com>
2024-08-01 11:59:21 -0400

    overrode install command to include [extra] by default

--- .spin/cmds.py
+++ .spin/cmds.py
@@ -201,3 +201,63 @@ def asv(asv_args):
     os.environ["ASV_ENV_DIR"] = "/Users/adam2392/miniforge3"
     os.environ["PYTHONPATH"] = f'{site_path}{os.sep}:{os.environ.get("PYTHONPATH", "")}'
     util.run(["asv"] + list(asv_args))
+
+
+@click.command()
+@click.option(
+    "--editable/--no-editable",
+    default=True,
+    help="Install in editable mode.",
+)
+@click.option(
+    "-v/-q",
+    "--verbose/--quiet",
+    is_flag=True,
+    default=True,
+    help="Print detailed build output.",
+)
+@click.option(
+    "--verbose-import/--no-verbose-import",
+    is_flag=True,
+    default=True,
+    help="Meson only: importing an editable install may trigger a build. This flag determines whether to print that build's output.",
+)
+@click.option(
+    "--with-extra/--no-extra",
+    is_flag=True,
+    default=True,
+    help="Install with extra dependencies.",
+)
+@click.argument("pip_args", nargs=-1)
+def install(*, pip_args, editable, verbose, verbose_import, with_extra):
+    """💽 Build and install package using pip.
+
+    By default, the package is installed in editable mode.
+
+    Arguments after `--` are passed through to pip, e.g.:
+
+      spin install -- --no-clean
+
+    would translated to:
+
+      pip install . --no-build-isolation --editable --no-clean
+    """
+    pip_args = list(pip_args)
+    pip_cmd = ["pip", "install"]
+    pip_args += ["--no-build-isolation"]
+
+    if editable:
+        pip_args += ["--editable"]
+
+        if verbose_import:
+            pip_args = ["--config-settings=editable-verbose=true"] + pip_args
+
+    if verbose:
+        pip_args = ["-v"] + pip_args
+
+    pkg = ["."]
+    if with_extra:
+        pkg[0] += "[extra]"
+
+    print(pip_cmd + pip_args + pkg)
+    util.run(pip_cmd + pip_args + pkg, sys_exit=False, replace=True)
